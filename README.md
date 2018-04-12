b3b42cf4413c98b3d7c93607cc194530ad4c3067
Herbert Li <hl1785@nyu.edu>
2018-04-11 20:08:29 -0400

    Fix to make universal-links work for iOS

--- README.md
+++ README.md
@@ -27,11 +27,12 @@ $ npm run build
 ```
 
 ### NewGamePortalPhonegap Checklist 
-* make sure cordova-plugin-whitelist is installed
-```
-$ cordova plugin add cordova-plugin-whitelist
-$ cordova prepare
-```
+* make sure following plugins are installed ($ cordova plugin add [plugin name]):
+  * cordova-plugin-whitelist
+  * cordova-sms-plugin
+  * cordova-plugin-contacts
+  * cordova-universal-links-plugin
+* cordova-universal-links-plugin breaks in iOS, use this fix: https://github.com/martindrapeau/cordova-universal-links-plugin/commit/b0f85438e4ef9d107be0aadeb218ae9c4131d02a#diff-084a996a8428527995ef522579929873
 
 * Run:
 ```
--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -1,9 +1,122 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "cordova-sms-plugin.Sms",
+    "file": "plugins/cordova-sms-plugin/www/sms.js",
+    "pluginId": "cordova-sms-plugin",
+    "clobbers": [
+      "window.sms"
+    ]
+  },
+  {
+    "id": "cordova-plugin-contacts.contacts",
+    "file": "plugins/cordova-plugin-contacts/www/contacts.js",
+    "pluginId": "cordova-plugin-contacts",
+    "clobbers": [
+      "navigator.contacts"
+    ]
+  },
+  {
+    "id": "cordova-plugin-contacts.Contact",
+    "file": "plugins/cordova-plugin-contacts/www/Contact.js",
+    "pluginId": "cordova-plugin-contacts",
+    "clobbers": [
+      "Contact"
+    ]
+  },
+  {
+    "id": "cordova-plugin-contacts.convertUtils",
+    "file": "plugins/cordova-plugin-contacts/www/convertUtils.js",
+    "pluginId": "cordova-plugin-contacts"
+  },
+  {
+    "id": "cordova-plugin-contacts.ContactAddress",
+    "file": "plugins/cordova-plugin-contacts/www/ContactAddress.js",
+    "pluginId": "cordova-plugin-contacts",
+    "clobbers": [
+      "ContactAddress"
+    ]
+  },
+  {
+    "id": "cordova-plugin-contacts.ContactError",
+    "file": "plugins/cordova-plugin-contacts/www/ContactError.js",
+    "pluginId": "cordova-plugin-contacts",
+    "clobbers": [
+      "ContactError"
+    ]
+  },
+  {
+    "id": "cordova-plugin-contacts.ContactField",
+    "file": "plugins/cordova-plugin-contacts/www/ContactField.js",
+    "pluginId": "cordova-plugin-contacts",
+    "clobbers": [
+      "ContactField"
+    ]
+  },
+  {
+    "id": "cordova-plugin-contacts.ContactFindOptions",
+    "file": "plugins/cordova-plugin-contacts/www/ContactFindOptions.js",
+    "pluginId": "cordova-plugin-contacts",
+    "clobbers": [
+      "ContactFindOptions"
+    ]
+  },
+  {
+    "id": "cordova-plugin-contacts.ContactName",
+    "file": "plugins/cordova-plugin-contacts/www/ContactName.js",
+    "pluginId": "cordova-plugin-contacts",
+    "clobbers": [
+      "ContactName"
+    ]
+  },
+  {
+    "id": "cordova-plugin-contacts.ContactOrganization",
+    "file": "plugins/cordova-plugin-contacts/www/ContactOrganization.js",
+    "pluginId": "cordova-plugin-contacts",
+    "clobbers": [
+      "ContactOrganization"
+    ]
+  },
+  {
+    "id": "cordova-plugin-contacts.ContactFieldType",
+    "file": "plugins/cordova-plugin-contacts/www/ContactFieldType.js",
+    "pluginId": "cordova-plugin-contacts",
+    "merges": [
+      ""
+    ]
+  },
+  {
+    "id": "cordova-plugin-contacts.contacts-ios",
+    "file": "plugins/cordova-plugin-contacts/www/ios/contacts.js",
+    "pluginId": "cordova-plugin-contacts",
+    "merges": [
+      "navigator.contacts"
+    ]
+  },
+  {
+    "id": "cordova-plugin-contacts.Contact-iOS",
+    "file": "plugins/cordova-plugin-contacts/www/ios/Contact.js",
+    "pluginId": "cordova-plugin-contacts",
+    "merges": [
+      "Contact"
+    ]
+  },
+  {
+    "id": "cordova-universal-links-plugin.universalLinks",
+    "file": "plugins/cordova-universal-links-plugin/www/universal_links.js",
+    "pluginId": "cordova-universal-links-plugin",
+    "clobbers": [
+      "universalLinks"
+    ]
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-sms-plugin": "0.1.11",
+  "cordova-plugin-contacts": "3.0.1",
+  "cordova-universal-links-plugin": "1.2.1"
 };
 // BOTTOM OF METADATA
 });
